38d199fe20d13ae0a7dc8901555cf3a68b72902f
root <walther@plannmarketing.com.br>
2021-04-04 01:00:02 +0000

    Optimized Editor

---
+++ src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          quote: 'I want to be single'
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
