5081269e6075660465107b1dfb08be64669c892f
root <walther@plannmarketing.com.br>
2021-04-07 14:48:27 +0000

    State & Getters

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,33 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Walther Neto' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+    events: [
+      { id: 1, text: '...', organizer: '...' },
+      { id: 2, text: '...', organizer: '...' },
+      { id: 3, text: '...', organizer: '...' },
+      { id: 4, text: '...', organizer: '...' },
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,10 +1,23 @@
 <template>
-    <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by #{{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(3) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-  export default {
+  import { mapState, mapGetters } from 'vuex'
 
+  export default {
+    computed: {
+      ...mapGetters(['catLength', 'getEventById']),
+      ...mapState(['user', 'categories'])
+    }
   }
 </script>
 
